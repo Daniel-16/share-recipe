ff7534469110a9bce3f42dd3db037a43f135bff6
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-08 00:06:39 +0100

    feat: middleware for routes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./db.js";
 import "dotenv/config.js";
+import router from "./routes/routes.js";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
 const port = process.env.PORT || 6000;
 try {
