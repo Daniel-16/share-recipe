f7eda9abc2594740b046d713e9a550e93d334f14
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-09 00:11:47 +0100

    routes for recipe model

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { createUser } from "../controllers/UserController.js";
+import { createRecipe } from "../controllers/RecipeController.js";
 const router = express.Router();
 
 router.post("/signup", createUser);
+router.post("/createRecipe", createRecipe);
 
 export default router;
