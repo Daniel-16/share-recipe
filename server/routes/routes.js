3933a22aa025af2730dafab6642b662e19c7886c
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-10 00:44:48 +0100

    feat: code doc for routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,29 @@ import { createUser } from "../controllers/UserController.js";
 import { createRecipe } from "../controllers/RecipeController.js";
 const router = express.Router();
 
+/**
+ * Route for user sign-up.
+ * POST /api/signup
+ * @name signup
+ * @memberof router
+ * @function
+ * @inner
+ * @param {string} path - Express path.
+ * @param {callback} middleware - Express middleware.
+ */
 router.post("/signup", createUser);
+
+/**
+ * Route for creating a recipe.
+ * POST /api/createRecipe/:recipeOwnerId
+ * @name createRecipe
+ * @memberof router
+ * @function
+ * @inner
+ * @param {string} path - Express path.
+ * @param {callback} middleware - Express middleware.
+ * @param {string} param - Recipe owner ID.
+ */
 router.post("/createRecipe/:recipeOwnerId", createRecipe);
 
 export default router;
