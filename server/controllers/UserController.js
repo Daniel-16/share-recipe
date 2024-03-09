6b37239710055bca50b11222f7520a9fd7ca11e6
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-10 00:42:53 +0100

    feat: code doc for creating user

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,14 @@
 import UserModel from "../models/UserModel.js";
 // import RecipeModel from "../models/RecipeModel.js";
 
+/**
+ * Creates a new user.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} The created user.
+ */
 export const createUser = async (req, res) => {
+  // Data from the request body
   const { username, email, password } = req.body;
   try {
     const user = await UserModel.create({
