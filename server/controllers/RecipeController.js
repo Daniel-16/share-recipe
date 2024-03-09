48e32edfbb13ca1517976166e9128fd85f43da8d
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-10 00:41:46 +0100

    feat: create recipe code doc

--- server/controllers/RecipeController.js
+++ server/controllers/RecipeController.js
@@ -1,8 +1,16 @@
 import RecipeModel from "../models/RecipeModel.js";
 import UserModel from "../models/UserModel.js";
 
+/**
+ * Creates a new recipe.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} The created recipe.
+ */
 export const createRecipe = async (req, res) => {
+  // Data extraction from the request body
   const { title, imageUrl, timeFrame, instructions } = req.body;
+  // Extracting the recipeOwnerId from the request paramaters
   const { recipeOwnerId } = req.params;
   try {
     const user = await UserModel.findById(recipeOwnerId);
