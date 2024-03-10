6b2ff585ec36c8ee32ca01af62361a2c109cb5b0
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-10 15:00:45 +0100

    feat(auth): add login user controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,6 @@
 import UserModel from "../models/UserModel.js";
 // import RecipeModel from "../models/RecipeModel.js";
+import bcrypt from "bcrypt";
 
 /**
  * Creates a new user.
@@ -22,6 +23,41 @@ export const createUser = async (req, res) => {
     });
   } catch (error) {
     res.status(401).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //Find user from db if they exist
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalid email or password",
+      });
+    }
+    //Match passwords
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalid email or password",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
       success: false,
       error: error.message,
     });
