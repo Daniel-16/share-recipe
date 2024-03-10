535ca0a9bea2a3c249ba49909fd4f84cc4c41975
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-10 15:42:38 +0100

    feat: Add middleware to handle JSON syntax errors

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import connectDB from "./db.js";
 import "dotenv/config.js";
 import router from "./routes/routes.js";
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 6000;
 try {
