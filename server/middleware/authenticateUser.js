bd328cfb82c58ace9a38a5f9a732997897d108d8
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-10 16:44:45 +0100

    docs: Add JSDoc comments to document auth user middleware

--- server/middleware/authenticateUser.js
+++ server/middleware/authenticateUser.js
@@ -1,6 +1,15 @@
 import jwt from "jsonwebtoken";
 import UserModel from "../models/UserModel.js";
 
+/**
+ * Middleware function to authenticate user using JWT token.
+ * Checks the authorization header for a JWT token, verifies it, and attaches the authenticated user to the request object.
+ * If the token is valid, proceeds to the next middleware function. Otherwise, returns a 401 error response.
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {function} next - The next middleware function in the request-response cycle.
+ * @returns {void} - Does not return a value directly, but may modify the request object or send a response.
+ */
 export const authenticateUser = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
