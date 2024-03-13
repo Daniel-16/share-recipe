8a0b4db3ef9116af1a1c5a71b8e20dc078e9aaf9
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-13 21:16:58 +0100

    change i don't know

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,14 +2,15 @@
 import { useState } from "react";
 import Image from "next/image";
 import foodMain from "../assets/michele-blackwell-rAyCBQTH7ws-unsplash.jpg";
+import Link from "next/link";
 
 function Home() {
   const [state, setState] = useState(false);
   const navigation = [
-    { title: "Features", path: "javascript:void(0)" },
-    { title: "Integrations", path: "javascript:void(0)" },
-    { title: "Customers", path: "javascript:void(0)" },
-    { title: "Pricing", path: "javascript:void(0)" },
+    { title: "Home", path: "javascript:void(0)" },
+    { title: "Recipes", path: "javascript:void(0)" },
+    { title: "About us", path: "javascript:void(0)" },
+    { title: "Contact", path: "javascript:void(0)" },
   ];
 
   // useEffect(() => {
@@ -89,7 +90,7 @@ function Home() {
                 : ""
             }`}
           >
-            <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+            <div className="gap-x-14 items-center max-w-screen-2xl mx-10 px-4 md:flex md:px-8">
               <Brand />
               <div
                 className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
@@ -110,87 +111,73 @@ function Home() {
                     );
                   })}
                 </ul>
-                <div className="items-center justify-end mt-6 space-y-6 md:flex md:mt-0">
-                  <a
-                    href="javascript:void(0)"
-                    className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
-                  >
-                    Sign in
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      className="w-5 h-5"
+                <div className="md:space-x-2">
+                  <div className="items-center justify-end mt-6 space-y-6 space-x-4 md:inline-flex md:mt-0">
+                    <Link
+                      href="/"
+                      className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
+                      Log in
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </Link>
+                    div
+                  </div>
+                  <div className="items-center justify-end mt-6 space-y-6 space-x-4 md:inline-flex md:mt-0">
+                    <Link
+                      href="/"
+                      className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
+                    >
+                      Sign up
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
           </nav>
         </header>
         <section>
-          <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 overflow-hidden md:px-8 md:flex">
+          <div className="max-w-screen-lg mx-auto px-4 py-20 gap-12 text-gray-600 overflow-hidden md:px-4 md:flex">
             <div className="flex-none space-y-5 max-w-xl">
-              <a
-                href="javascript:void(0)"
-                className="inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white"
-              >
-                <span className="inline-block rounded-full px-3 py-1 bg-indigo-600 text-white">
-                  News
-                </span>
-                <p className="flex items-center">
-                  Read the launch post from here
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-5 h-5"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </p>
-              </a>
-              <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl">
-                Build your SaaS exactly how you want
+              <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl md:pt-10">
+                Delicious Dishes for Food Bloggers
               </h1>
               <p>
                 Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
-                doloremque laudantium, totam rem aperiam, eaque ipsa quae.
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+                voluptas. Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Veniam illo officiis odio temporibus vero at eius facilis
+                aspernatur ipsa necessitatibus?
               </p>
               <div className="flex items-center gap-x-3 sm:text-sm">
                 <a
                   href="javascript:void(0)"
                   className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
                 >
-                  Get started
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-5 h-5"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-                <a
-                  href="javascript:void(0)"
-                  className="flex items-center justify-center gap-x-1 py-2 px-4 text-gray-700 hover:text-gray-900 font-medium duration-150 md:inline-flex"
-                >
-                  Contact sales
+                  Explore recipes
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
@@ -209,8 +196,9 @@ function Home() {
             <div className="flex-1 hidden md:block">
               <Image
                 src={foodMain}
-                className="max-w-xl md:rounded-tl-[108px]"
+                className="md:rounded-tl-[108px] md:rounded-br-[108px] w-auto h-auto"
                 alt="Food main"
+                // style={{ height: "20rem" }}
               />
             </div>
           </div>
