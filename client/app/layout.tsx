34014a7d65918a07bb4070369fe8318eee3a1f58
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-14 14:32:45 +0100

    changed metadata

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Recipe Blogs",
+  title: "Cook",
   description: "Web app for sharing recipes",
 };
 
