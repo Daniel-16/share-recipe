d1f478ee290eabbd998397f01ffe8a95d89e4c7a
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-14 14:40:43 +0100

    fix(layout): background color

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         className={inter.className}
         style={{
           background:
-            "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)",
+            "linear-gradient(143.6deg, rgba(192, 132, 252, 0)20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)",
         }}
       >
         {children}
