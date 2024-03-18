f672bb21e75dcd112249e51e22d538505b8f4712
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-18 01:52:50 +0100

    feat: section context

---
+++ client/context/SectionContext.tsx
@@ -0,0 +1,15 @@
+// "use client";
+// import { createContext, useRef } from "react";
+
+// // export const SectionContext = createContext();
+
+// export const SectionProvider = ({ children }: { children: any }) => {
+//   const section1 = useRef(null);
+//   const section2 = useRef(null);
+//   const section3 = useRef(null);
+//   return (
+//     <SectionContext.Provider value={{ section1, section2, section3 }}>
+//       {children}
+//     </SectionContext.Provider>
+//   );
+// };
