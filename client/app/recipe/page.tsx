137c2dd45cfea03fac8e976fab957d60e254993e
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-20 19:33:07 +0100

    fix: flex-shrink for avatar

--- client/app/recipe/page.tsx
+++ client/app/recipe/page.tsx
@@ -42,7 +42,7 @@ export default function Recipe() {
             <div className="inline-flex items-center space-x-2">
               <svg
                 fill="#B66055"
-                className="w-6"
+                className="w-6 flex-shrink-0"
                 viewBox="0 0 512 512"
                 id="_x30_1"
                 version="1.1"
@@ -52,7 +52,9 @@ export default function Recipe() {
               >
                 <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,90  c37.02,0,67.031,35.468,67.031,79.219S293.02,248.438,256,248.438s-67.031-35.468-67.031-79.219S218.98,90,256,90z M369.46,402  H142.54c-11.378,0-20.602-9.224-20.602-20.602C121.938,328.159,181.959,285,256,285s134.062,43.159,134.062,96.398  C390.062,392.776,380.839,402,369.46,402z" />
               </svg>
-              <p className="text-gray-800 text-sm font-semibold">Username</p>
+              <p className="text-gray-800 text-sm font-semibold overflow-auto">
+                username
+              </p>
             </div>
             <div className="inline-flex items-center space-x-2">
               <svg
