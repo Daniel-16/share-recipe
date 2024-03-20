e94879cc832ed383747764746e1c143d53a9486a
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-20 18:40:07 +0100

    feat: ingredients

--- client/app/recipe/components/RecipeDetails.tsx
+++ client/app/recipe/components/RecipeDetails.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import foodImage from "../../../assets/michele-blackwell-rAyCBQTH7ws-unsplash.jpg";
+import Ingredients from "./Ingredients";
 
 export default function RecipeDetails() {
   return (
@@ -9,10 +10,20 @@ export default function RecipeDetails() {
         src={foodImage}
         alt="Food"
         className="w-auto h-[60vh] mx-auto object-contain "
+        priority
       />
       <div className="py-5 inline-flex items-center justify-center space-x-2">
-        <p className="text-gray-600 text-2xl font-extrabold">Cooking time:</p>
-        <span className="text-gray-800 text-2xl font-extrabold">10 mins</span>
+        <p className="text-gray-800 text-2xl font-extrabold">Cooking time:</p>
+        <span className="text-gray-600 text-2xl font-extrabold">10 mins</span>
+      </div>
+      <p className="text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+        similique sequi doloribus, vero quos ratione ex asperiores nostrum.
+        Voluptates, sit.
+      </p>
+      <div className="pt-10">
+        <h1 className="text-4xl font-extrabold text-gray-800">Ingredients:</h1>
+        <Ingredients />
       </div>
     </div>
   );
