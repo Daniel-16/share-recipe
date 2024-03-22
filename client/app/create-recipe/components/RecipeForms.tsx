969916449b52254268ee5ac8a09e35283514a96d
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-22 18:31:35 +0100

    file image form

--- client/app/create-recipe/components/RecipeForms.tsx
+++ client/app/create-recipe/components/RecipeForms.tsx
@@ -1,7 +1,9 @@
+import { Input } from "@/components/ui/input";
+
 export default function RecipeForm() {
   return (
     <>
-      <form>
+      <form className="space-y-14">
         <div>
           <label className="text-3xl text-gray-800 font-medium">
             Recipe Title:
@@ -12,6 +14,12 @@ export default function RecipeForm() {
             className="w-full mt-2 px-3 py-3 text-gray-800 outline-none border focus:border-[#B66070] shadow-md rounded-lg duration-200"
             placeholder="Chocolate Chip Cookies"
           />
+        </div>
+        <div>
+          <label className="text-3xl text-gray-800 font-medium">
+            Recipe Image:
+          </label>
+          <Input type="file" id="picture" accept=".jpg, .webp, .jpeg, .png" />
         </div>
       </form>
     </>
