cc00db3033e99734def1034f4055f8bf24775f76
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-22 18:12:39 +0100

    create recipe page

---
+++ client/app/create-recipe/components/RecipeForms.tsx
@@ -0,0 +1,19 @@
+export default function RecipeForm() {
+  return (
+    <>
+      <form>
+        <div>
+          <label className="text-3xl text-gray-800 font-medium">
+            Recipe Title:
+          </label>
+          <input
+            type="text"
+            required
+            className="w-full mt-2 px-3 py-3 text-gray-800 outline-none border focus:border-[#B66070] shadow-md rounded-lg duration-200"
+            placeholder="Chocolate Chip Cookies"
+          />
+        </div>
+      </form>
+    </>
+  );
+}
---
+++ client/app/create-recipe/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar";
+import { Separator } from "@/components/ui/separator";
+import RecipeForm from "./components/RecipeForms";
+
+export default function CreateRecipe() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-screen-lg px-5 mx-auto">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl text-gray-800 font-extrabold md:text-4xl">
+            Create new recipe
+          </h1>
+          <button className="bg-[#B66055] text-white py-2 px-4 rounded-lg shadow-md hover:bg-[#A7727D]">
+            Save
+          </button>
+        </div>
+        <Separator className="mt-10 mb-10" />
+        <RecipeForm />
+      </div>
+    </>
+  );
+}
