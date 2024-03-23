5accd8956bfd45408f1521a960dec3d19ceeb040
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-23 13:27:50 +0100

    link to create recipe

--- client/components/Section.tsx
+++ client/components/Section.tsx
@@ -32,7 +32,7 @@ export default function Section() {
         </p>
         <div className="flex items-center gap-x-3 sm:text-sm">
           <Link
-            href=""
+            href="/create-recipe"
             className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
           >
             Create a recipe
