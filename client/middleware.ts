20c31d46c127432d5c39eeae44798b93433185bc
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-25 00:13:21 +0100

    feat: allow user only visit home page

--- client/middleware.ts
+++ client/middleware.ts
@@ -7,8 +7,21 @@ export function middleware(request: NextRequest) {
     return Response.redirect(new URL("/", request.url));
   }
 
-  if (!currentUser && !request.nextUrl.pathname.startsWith("/login")) {
-    return Response.redirect(new URL("/login", request.url));
+  if (currentUser) {
+    if (!currentUser && !request.nextUrl.pathname.startsWith("/login")) {
+      return Response.redirect(new URL("/login", request.url));
+    }
+  }
+  if (!currentUser) {
+    // If the user is not authenticated
+    if (
+      !request.nextUrl.pathname.startsWith("/login") &&
+      !request.nextUrl.pathname.startsWith("/signup") &&
+      !request.nextUrl.pathname.endsWith("/")
+    ) {
+      // If the user is trying to access any page other than /login or /signup, redirect to /login
+      return Response.redirect(new URL("/login", request.url));
+    }
   }
 }
 
