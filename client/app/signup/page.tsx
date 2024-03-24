05f876aedca19090ccb51d43adc11dad4a67148e
Daniel-16 <danieloloruntoba681@gmail.com>
2024-03-25 00:52:00 +0100

    feat: signup a user

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,9 +1,49 @@
 "use client";
-
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
+import { useState, useContext } from "react";
+import Axios from "axios";
+import { useRouter } from "next/navigation";
+import { AuthContext } from "@/context/AuthContext";
 
 export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await Axios.post(
+        "http://localhost:7000/api/signup",
+        {
+          username,
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.data.token) {
+        document.cookie = `currentUser=${response.data.token}; path=/; secure-${
+          process.env.NODE_ENV === "production"
+        }; sameSite=strict`;
+        router.push("/");
+        setIsAuthenticated(true);
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="h-screen">
       <Navbar />
@@ -26,7 +66,7 @@ export default function Signup() {
             </div>
           </div>
           <div className="bg-white shadow p-4 py-6 sm:p-6 rounded-[15px] md:rounded-lg">
-            <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+            <form onSubmit={handleSubmit} className="space-y-5">
               <div>
                 <label className="font-medium">Username</label>
                 <input
@@ -34,6 +74,8 @@ export default function Signup() {
                   required
                   className="w-full mt-2 px-3 py-2 text-gray-800 bg-transparent outline-none border focus:border-[#B66070] shadow-md rounded-lg duration-200"
                   placeholder="johndoe"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
               <div>
@@ -43,6 +85,8 @@ export default function Signup() {
                   required
                   className="w-full mt-2 px-3 py-2 text-gray-800 bg-transparent outline-none border focus:border-[#B66070] shadow-md rounded-lg duration-200"
                   placeholder="johndoe@mail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div>
@@ -52,9 +96,14 @@ export default function Signup() {
                   required
                   className="w-full mt-2 px-3 py-2 text-gray-800 bg-transparent outline-none border focus:border-[#B66070] shadow-md rounded-lg duration-200"
                   placeholder="Enter a secure password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <button className="w-full px-4 py-2 text-white font-medium bg-[#a7727d] hover:bg-[#B66070] active:bg-[#a7727d] rounded-lg duration-150">
+              <button
+                type="submit"
+                className="w-full px-4 py-2 text-white font-medium bg-[#a7727d] hover:bg-[#B66070] active:bg-[#a7727d] rounded-lg duration-150"
+              >
                 Create account
               </button>
             </form>
